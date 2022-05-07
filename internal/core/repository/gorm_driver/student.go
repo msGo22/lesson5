33a68a1500f3fdd13077c20400f0ff5902632803
Alameddin Çelik <alameddinc@gmail.com>
2022-05-07 19:48:00 +0300

    Updated Mocks

---
+++ internal/core/repository/gorm_driver/student.go
@@ -0,0 +1,31 @@
+package gorm_driver
+
+import (
+	"gorm.io/gorm"
+	"week5/internal/core/domains"
+)
+
+type StudentGormRepo struct {
+	db *gorm.DB
+}
+
+func NewStudentRepo(db *gorm.DB) (*StudentGormRepo, error) {
+	if err := db.AutoMigrate(&domains.Student{}); err != nil {
+		return nil, err
+	}
+	return &StudentGormRepo{
+		db: db,
+	}, nil
+}
+
+func (s *StudentGormRepo) Save(student *domains.Student) error {
+	return s.db.Save(student).Error
+}
+
+func (s *StudentGormRepo) GetByID(ID uint) (*domains.Student, error) {
+	var student domains.Student
+	if err := s.db.Find(&student, ID).Error; err != nil {
+		return nil, err
+	}
+	return &student, nil
+}
--- internal/core/repository/student.go
+++ internal/core/repository/student.go
@@ -1,31 +1,8 @@
 package repository
 
-import (
-	"gorm.io/gorm"
-	"week5/internal/core/domains"
-)
+import "week5/internal/core/domains"
 
-type StudentRepo struct {
-	db *gorm.DB
-}
-
-func NewStudentRepo(db *gorm.DB) (*StudentRepo, error) {
-	if err := db.AutoMigrate(&domains.Student{}); err != nil {
-		return nil, err
-	}
-	return &StudentRepo{
-		db: db,
-	}, nil
-}
-
-func (s *StudentRepo) Save(student *domains.Student) error {
-	return s.db.Save(student).Error
-}
-
-func (s *StudentRepo) GetByID(ID uint) (*domains.Student, error) {
-	var student domains.Student
-	if err := s.db.Find(&student, ID).Error; err != nil {
-		return nil, err
-	}
-	return &student, nil
+type StudentRepo interface {
+	Save(student *domains.Student) error
+	GetByID(ID uint) (*domains.Student, error)
 }
